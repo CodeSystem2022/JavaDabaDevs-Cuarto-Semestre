2559fa0c37d9ced0af21a82ad325e085ba4bdc28
David <estechedavid@outlook.com.ar>
2023-10-11 11:19:50 -0300

    E-commerce. Clase 1 a 4
    Co-Authored-By: Marita <113071029+vrunito2021@users.noreply.github.com>
    Co-Authored-By: Marcelo Alejandro Boujón <77926233+boujonmarcelo@users.noreply.github.com>
    Co-Authored-By: Carolina Mamani <111472448+CMCNY@users.noreply.github.com>
    Co-Authored-By: Augustonc <73897032+Augustonc@users.noreply.github.com>
    Co-Authored-By: JohanaMM <89425234+JohanaMM@users.noreply.github.com>
    Co-Authored-By: pilochincho <98560604+pilochincho@users.noreply.github.com>

---
+++ JavaScript/E-commerce/client/js/products.js
@@ -0,0 +1,33 @@
+const productos = [
+    {
+        id: 1,
+        productName: 'Lapices',
+        price: 480,
+        quanty: 1,
+        img: "/client/media/lapices.png",
+    },
+    {
+        id: 2,
+        productName: 'Cuaderno Doble',
+        price: 950,
+        quanty: 1,
+        img: "/client/media/cuadernoDoble.png",
+    },
+
+    {
+
+        id: 3,
+        productName: 'Dos Mochilas',
+        price: 750,
+        quanty: 2,
+        img: "/client/media/dosMochi.png"
+    },
+
+    {
+        id: 4,
+        productName: 'Libros',
+        price: 5000,
+        quanty: 5,
+        img: "/client/media/libros.png"
+    }
+]
