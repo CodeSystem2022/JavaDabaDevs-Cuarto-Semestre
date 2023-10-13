d11109bc9e5c6f746951c300670609fa4e9c3900
Marcelo Alejandro Boujón <77926233+boujonmarcelo@users.noreply.github.com>
2023-10-13 12:04:40 -0300

    Update app2.js

--- Node/Semana 3/01-HolaMundo/app2.js
+++ Node/Semana 3/01-HolaMundo/app2.js
@@ -3,4 +3,4 @@ const saludar = (nombre) => { //función flecha asignada a una constante
     return `saludos de ${nombre}`; //usamos backtic's para hacer un template string
 } // esto es igual a 'saludos' + nombre;
 
-console.log(saludar('NoeliaCarolina'));
+console.log(saludar('Carolina'));
